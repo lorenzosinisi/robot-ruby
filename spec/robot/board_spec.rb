bbcb9b86f0cffb4082426a8f59d9f334eb1c84de
Lorenzo Sinisi <lasslo.net@gmail.com>
2015-12-10 09:33:08 +0100

    DRY tests with before each hook

--- spec/robot/board_spec.rb
+++ spec/robot/board_spec.rb
@@ -2,44 +2,53 @@ require 'spec_helper'
 
 module Robot
   describe Board do
+    
+    before(:each) do 
+      @board  = Robot::Board.new
+    end
+
     it "should be a Robot module" do 
       expect(Board).to be Robot::Board
     end
     
     context "#initialize" do
       it "is expected to be itialized with a 2d array of 5x5 by default" do
-        board = Robot::Board.new
-        expect(board.grid).to eq Array.new(5) { Array.new(5) {  }  }
+        expect(@board.grid).to eq Array.new(5) { Array.new(5) {  }  }
       end
 
       it "is expected to be itialized with the position 0x0 by default" do
-        board = Robot::Board.new
-        expect(board.position).to eq [0,0]
+        expect(@board.position).to eq [0,0]
       end
 
       it "is expected to be itialized with the position 0x0 by default" do
-        board = Robot::Board.new
-        expect(board.direction).to eq "south"
+        expect(@board.direction).to eq "south"
       end
     end
 
     context "#get cell" do
       it "returns the cell based on the (x, y) coordinates" do
-        board = Robot::Board.new
-        expect(board.get_cell(2,2)).to eq(nil)
+        expect(@board.get_cell(2,2)).to eq(nil)
       end
     end
 
     context "#set cell" do
       it "sets the value of cell based on the (x, y) coordinates" do
-        board = Robot::Board.new
-        expect(board.set_cell(2,2, "right")).to eq("right")
+        expect(@board.set_cell(2,2, "right")).to eq("right")
       end
 
       it "should also set the new position of the robot" do
-        board = Robot::Board.new
-        board.set_cell(2,2, "right")
-        expect(board.position).to eq [2,2]
+        @board.set_cell(2,2, "right")
+        expect(@board.position).to eq [2,2]
+      end
+    end
+
+    context "#current x and current y" do
+      it "returns the current x" do
+        expect(@board.current_x).to eq(0)
+      end
+
+      it "should also set the new position of the robot" do
+        expect(@board.current_y).to eq(0)
       end
     end
 
