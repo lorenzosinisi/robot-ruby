683d03f418c0daf117d44fa7d83bb211a045b8d8
Lorenzo Sinisi <lasslo.net@gmail.com>
2015-12-11 03:27:16 +0100

    changed way the files are loaded

--- lib/robot.rb
+++ lib/robot.rb
@@ -1,6 +1,5 @@
-require "robot/version"
-require "robot/play"
-require "robot/board"
-
 module Robot
 end
+
+lib_path = File.expand_path(File.dirname(__FILE__))
+Dir[lib_path + "/robot/**/*.rb"].each { |file| require file }
