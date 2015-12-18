c917ecdc19fde3bd1723ff26a58aa44e720194ec
Lorenzo Sinisi <lasslo.net@gmail.com>
2015-12-18 11:59:28 +0100

    added attributes x and y to the board

--- lib/robot/board.rb
+++ lib/robot/board.rb
@@ -1,4 +1,12 @@
 module Roboruby
   class Board
+
+    attr_accessor :x, :y
+
+    def initialize(options={})
+      @x = options[:x] || 4
+      @y = options[:y] || 4
+    end
+
   end
 end
--- spec/robot/board_spec.rb
+++ spec/robot/board_spec.rb
@@ -2,8 +2,15 @@ require 'spec_helper'
 
 module Roboruby
   describe Board do
-    it "should be a Robot module" do
-      expect(Board).to be Roboruby::Board
+    let!(:board) { Roboruby::Board.new }
+    context "#initialize" do
+      it "should be a Robot module" do
+        expect(Board).to be Roboruby::Board
+      end
+      it "have x and y by default" do
+        expect(board.x).to be 4
+        expect(board.y).to be 4
+      end
     end
   end
 end
