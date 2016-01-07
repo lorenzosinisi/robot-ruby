4c0fcebae029cad722ec2ecb084b204e954f3f53
Lorenzo Sinisi <lasslo.net@gmail.com>
2016-01-08 00:36:34 +0100

    added some more comments and todos

--- lib/robot/robot.rb
+++ lib/robot/robot.rb
@@ -1,8 +1,8 @@
 module Roboruby
   class Robot
 
-    attr_accessor :current_x, :current_y, :direction
-    attr_reader :board
+    attr_reader :board, :compass # use Board and Compass in Robot memory
+    attr_reader :current_x, :current_y, :direction # Store current position in robot memory
 
     def initialize(options={})
       @board     = options[:board] || Roboruby::Board.new
@@ -12,39 +12,40 @@ module Roboruby
       @direction = 0
     end
 
+    # Place should be the only method able to
+    # assign new values to x,y and f of the robot
     def place(x,y, value = direction)
       if board.in_grid?(x,y)
-        self.direction = value
-        self.current_x = x
-        self.current_y = y
+        @current_x, @current_y, @direction = x, y, value
         true
       end
     end
 
     def move(*)
-      dir = @compass.grad_to_direction(direction)
-      new_value = @compass.pointing[dir]
+      dir = compass.grad_to_direction(direction)
+      new_value = compass.pointing[dir]
       place(current_x + new_value[:x], current_y + new_value[:y])
     end
 
     def right(*)
-      new_direction = @compass.rotate[:right]
+      new_direction = compass.rotate[:right]
       place(current_x, current_y, direction + new_direction)
     end
 
     def left(*)
-      new_direction = @compass.rotate[:left]
+      new_direction = compass.rotate[:left]
       place(current_x, current_y, direction + new_direction)
     end
 
     def report(*)
-      status = [current_x, current_y, human_direction]
+      status = [current_x, current_y, human_direction] # TODO use human_direction implemented in Compass instead
       puts status
       status
     end
-
+    
+    # TODO remove this method from here and move it to Compass
     def human_direction
-      @compass.grad_to_direction(direction).upcase.to_s
+      compass.grad_to_direction(direction).upcase.to_s
     end
 
   end
