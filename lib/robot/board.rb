b1664ba3dd4764d60b68a2f214ae32cb9f836f23
Lorenzo Sinisi <lasslo.net@gmail.com>
2016-02-23 19:38:54 +1100

    better style for class Board

--- lib/robot/board.rb
+++ lib/robot/board.rb
@@ -1,19 +1,19 @@
 module Roboruby
   class Board
-    ## Board
-    # should have axis x and y by default 4 and 4 that means being big 5x5
     attr_reader :width, :height, :size
 
     def initialize(options={})
-      @size   = options[:size] || 4
-      @width  = valid_axis(options[:width]) || size
-      @height = valid_axis(options[:height]) || size
+      default_size = 4
+      @size   = are_axis_valid? options[:size]   || default_size
+      @width  = are_axis_valid? options[:width]  || default_size
+      @height = are_axis_valid? options[:height] || default_size
     end
 
-    # check that the borad doesn't have negative numbers as size
-    def valid_axis(size)
-      return nil if size == nil
-      size = size >= 0 ? size : 0
+    private
+
+    def are_axis_valid?(size)
+      return nil unless size # so we can use the default value
+      size.abs # we want always a positive number for valid axis
     end
 
   end
